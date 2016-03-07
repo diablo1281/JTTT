58a07ce485a65d7adc7cc3a62f0bd8d3889481e5
Jędrzej Szczepaniak <je.szczepaniak@gmail.com>
2016-03-07 21:56:30 +0100

    Wysyłanie maili działa

--- JTTT/Form1.cs
+++ JTTT/Form1.cs
@@ -26,14 +26,32 @@ namespace JTTT
             string match_word = textBoxText.Text;
             string mail = textBoxMail.Text;
             string main_mail = "dotnet.JS.BR@gmail.com";
+            string pass = "Random123";
 
             Debug.WriteLine("URL: " + url);
             Debug.WriteLine("Word: " + match_word);
             Debug.WriteLine("Mail: " + mail);
 
-            MailMessage message = new MailMessage(main_mail, mail);
-            
+            //Ustawianie adresów mailowych
+            MailAddress from_address = new MailAddress(main_mail, "Nasz piękny program");
+            MailAddress to_address = new MailAddress(mail, "Client");
+            MailAddress replay_address = new MailAddress(main_mail);
 
+            MailMessage message = new MailMessage(from_address, to_address);
+            message.ReplyToList.Add(replay_address);
+
+            //Ustawianie tematu
+            message.Subject = "Szukałeś słowa \"" + match_word;
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            //Ustawianie treści
+            message.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+
+            //Treść w htmlu?
+            message.IsBodyHtml = true;
+
+            SendMail(message, main_mail, pass);
         }
 
         private static void SendMail(MailMessage message, string mail, string pass)
@@ -41,6 +59,7 @@ namespace JTTT
             SmtpClient client = new SmtpClient();
             client.Host = "smtp.googlemail.com";
             client.Port = 587;
+            client.EnableSsl = true;
             client.UseDefaultCredentials = false;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.EnableSsl = true;
